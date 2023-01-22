a911c25a6fd0df01e31266b654ee4301531f1ece
Joshua Taylor <toasterrage@gmail.com>
2023-01-22 13:37:16 -0700

    Updated server.js file

---
+++ server.js
@@ -0,0 +1,41 @@
+// *****Import Modules*****
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// *****Global Variables*****
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
+// Session cookie
+const sess = {
+    secret: 'Super secret statement here',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+// *****Express*****
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.use(session(sess));
+
+// // *****Handlebars*****
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
