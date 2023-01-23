3c07ee1aaa0202f835529f83b0a5d387a84935f3
Joshua Taylor <toasterrage@gmail.com>
2023-01-22 17:32:53 -0700

    Added db models

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+// *****Import Modules*****
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model { }
+
+// *****DB Table*****
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    content: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    /*
+    created_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+    },
+    */
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,56 @@
+// *****Import Modules*****
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+// *****DB Table*****
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [280]
+      }
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    /*
+    created_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+    },
+    */
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+
+module.exports = Post;
