131afd95b05698208b2a036f448a1d287b86e6a4
Joshua Taylor <toasterrage@gmail.com>
2023-01-25 16:39:38 -0700

    Homepage working

--- README.md
+++ README.md
@@ -46,7 +46,8 @@ If you followed tutorials, include links to those here as well.
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+Refer to the LICENSE in the repo for the code. 
+The font (Montserrat) is licensed under the SIL Open Font License (OFL).
 
 ---
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,22 +9,13 @@ router.get('/', async (req, res) => {
       include: [{ model: User }]
     });
     const posts = postsData.map((post) => post.get({ plain:true }));
+    
     // Remove sensitive data before sending over to user
     posts.forEach(post => {
       delete post.user.email;
       delete post.user.password;
     });
 
-    console.info(posts);
-    /*
-    posts.forEach(post => {
-      // Get user name by the user_id provided
-      const userName = User.findByPk(post.user_id);
-      post.user_id = userName.dataValues.name;
-      console.log(post.user_id);
-    });
-    */
-   console.info(posts);
     // Send handlebars page to user
     res.render('homepage', { posts });
   } catch(err) {
