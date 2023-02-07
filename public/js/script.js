b813d2dab843f201e0ffb4574eaa1367cc89e7ba
Joshua Taylor <toasterrage@gmail.com>
2023-02-07 14:50:38 -0700

    Added modal code to darken bg when open

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,30 @@
+// *****Global Variables*****
+let contentDarken = false;
+
 // *****Toggle Modals*****
 const toggleHidden = (id) => {
   document.getElementById(id).classList.toggle('hidden');
 }
 
+// Darken Content
+const darkenContent = (id) => {
+  document.getElementById(id).classList.toggle('darken');
+  document.getElementById('header-tag').classList.toggle('darken');
+}
+
+// Open modal
+const openModal = (modalId, contentId) => {
+  toggleHidden(modalId);
+  // Darken the rest of the content if not already darkened
+  if (!contentDarken) {
+    darkenContent(contentId);
+    contentDarken = true;
+  }
+}
+
 // Check for any open modals and close them
-const closeModals = () => {
-  console.log('closeModals hit');
+const closeModals = (contentId) => {
   const htmlSections = document.getElementsByTagName('section');
-  console.log(htmlSections);
   for (let i = 0; i < htmlSections.length; i++) {
     // Check for 'modal' in the beginning of the section id
     let modalCheck = (htmlSections[i].getAttribute('id').slice(0,5) === 'modal') ? true : false;
@@ -16,5 +33,10 @@ const closeModals = () => {
     if (modalCheck && !getClasses.includes('hidden')) {
       toggleHidden(htmlSections[i].getAttribute('id'));
     }
+  }
+  // Reverse darken for the rest of the content if not already darkened
+  if (contentDarken) {
+    darkenContent(contentId);
+    contentDarken = false;
   }
 }
