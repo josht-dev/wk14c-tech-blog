3b604430dd5cdc888fcdabc476fea1199c647a47
Joshua Taylor <toasterrage@gmail.com>
2023-02-08 15:58:41 -0700

    Added login route specifics

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,15 +1,52 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const bcrypt = require("bcrypt");
 
 // Post /api/users/ route to create a user
-router.post('/login', async (req, res) => {
-
+router.post('/', async (req, res) => {
+  try { }
+  catch (err) { }
 });
 
 // Post /api/users/login route to login
 router.post('/login', async (req, res) => {
+  try {
+    console.log('req body');
+    console.log(req.body);
 
+    const errMessage = 'Incorrect email or password!';
+
+    // Get user from db
+    const userData = await User.findOne({
+      raw: true,
+      where: {
+        email: req.body.email
+      }
+    });
+
+    console.log('userdata');
+    console.log(userData);
+
+    // Check for user in db
+    if (!userData) {
+      res.status(400).json({ message: errMessage });
+      return;
+    }
+
+    // Check for correct password
+    const validPw = await userData.checkPassword(req.body.password);
+    if (!validPw) {
+      res.status(400).json({ message: errMessage });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.redirect('../');
+    });
+
+
+  } 
+  catch (err) {res.status(500).json(err);}
 });
 
 // Post /api/users/logout route to logout
--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
+  checkPassword(pw) {
+    return bcrypt.compareSync(pw, this.password);
   }
 }
 
