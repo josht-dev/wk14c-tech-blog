2f36ea19a6188a6816e419731868d1fc2228998d
Joshua Taylor <toasterrage@gmail.com>
2023-02-09 11:52:16 -0700

    Added partials files and updated helper script

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,5 +2,13 @@ module.exports = {
   // Format the createdAt date/time
   format_date: (date) => {
     return date.toLocaleDateString();
+  },
+  // Check if 2 variables equal in handlebars
+  ifEquals: (a, b, options) => {
+    if (a === b) {
+      return options.fn(this);
+    }
+
+    return options.inverse(this);
   }
 };
